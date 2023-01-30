cad186b2eace97f6d5fa49fced4cf680b3390109
Carlosayol <Carlosaolar@gmail.com>
2023-01-30 13:46:40 -0500

    create first views for polls application

--- rankingapp/polls/views.py
+++ rankingapp/polls/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("You are now in the main page")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You are now looking at the question #{question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You are now looking at the results of the question #{question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are now voting for the question #{question_id}")
