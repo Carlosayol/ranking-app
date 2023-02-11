4c58455de7195f2acf4ac9402631ee34d0cb47be
Carlosayol <Carlosaolar@gmail.com>
2023-02-11 18:39:10 -0500

    add first test cases

--- rankingapp/polls/tests.py
+++ rankingapp/polls/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+# Models
+# Views
+
+
+class QuestionModelTests(TestCase):
+    def test_was_created_recently_with_future_questions(self):
+        """was_published_recently returns False for questions whose created_at is in the future"""
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="Test", created_at=time)
+        self.assertIs(future_question.was_created_recently(), False)
