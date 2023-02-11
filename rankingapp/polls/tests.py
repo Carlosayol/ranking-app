f41f6373c9500b1fe07e112d6cdd5130c7395f07
Carlosayol <Carlosaolar@gmail.com>
2023-02-11 18:58:10 -0500

    add full test suite for question model

--- rankingapp/polls/tests.py
+++ rankingapp/polls/tests.py
@@ -16,3 +16,17 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text="Test", created_at=time)
         self.assertIs(future_question.was_created_recently(), False)
+
+    def test_was_created_recently_with_past_questions(self):
+        """was_published_recently returns False for questions whose created_at is in the past"""
+
+        time = timezone.now() - datetime.timedelta(days=30)
+        future_question = Question(question_text="Test", created_at=time)
+        self.assertIs(future_question.was_created_recently(), False)
+
+    def test_was_created_recently_with_valid_questions(self):
+        """was_published_recently returns True for questions whose created_at is in the valid timerange"""
+
+        time = timezone.now() - datetime.timedelta(days=1)
+        future_question = Question(question_text="Test", created_at=time)
+        self.assertIs(future_question.was_created_recently(), True)
