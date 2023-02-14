ffb39cd48f6a442f20f18b4b7c51b0701678f3b6
Carlosayol <Carlosaolar@gmail.com>
2023-02-14 17:21:11 -0500

    customize basic options for admin

--- rankingapp/polls/admin.py
+++ rankingapp/polls/admin.py
@@ -5,12 +5,15 @@ from .models import Question, Choice
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 3
+    extra = 1
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fields = ["question_text"]
     inlines = [ChoiceInline]
+    list_display = ("question_text", "created_at", "was_created_recently")
+    list_filter = ["created_at"]
+    search_fields = ["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
